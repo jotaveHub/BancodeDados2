76bb8713a8072eaa0cbe5c984e2ec533bbb48335
jotaveHub <accurategx@gmail.com>
2026-09-09 16:58:32 -0300

    feat: criando .env e isolando credenciais do banco

--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,17 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
 import express from 'express'
 const app = express()
 const port = 3000
 
 import { Client } from 'pg'
 const client = await new Client({
-    host: 'localhost',
-    port: '5432',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'aula',
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
 }).connect()
  
 const res = await client.query('SELECT $1::text as message', ['Hello world!'])
